ce2381facc4419801e82f5ecc27302a7cd9b5fd9
arielmerin <arielmerino@ciencias.unam.mx>
2022-06-02 19:36:24 -0500

    🎉 Begin with last practice from algorithms course

---
+++ datastructures/Graph.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+from datastructures.MinHeap import MinHeap, print_array
+
+
+class Graph:
+
+    def __init__(self, vertex):
+        self.V = vertex
+        self.graph = defaultdict(list)
+
+    def add_edge(self, src, dest, weight):
+        new_node = [dest, weight]
+        self.graph[src].insert(0, new_node)
+        new_node = [src, weight]
+        self.graph[dest].insert(0, new_node)
+
+    def Prim(self):
+        V = self.V
+        key = []
+        parent = []
+
+        min_heap = MinHeap()
+
+        for v in range(V):
+            parent.append(-1)
+            key.append(9e9)
+            min_heap.array.append(min_heap.new_min_heap_node(v, key[v]))
+            min_heap.pos.append(v)
+
+        min_heap.pos[0] = 0
+        key[0] = 0
+        min_heap.decrease_key(0, key[0])
+
+        min_heap.size = V
+
+        while not min_heap.is_empty():
+            new_heap_node = min_heap.extract_min()
+            u = new_heap_node[0]
+
+            for pCrawl in self.graph[u]:
+                v = pCrawl[0]
+                if min_heap.is_in_minheap(v) and pCrawl[1] < key[v]:
+                    key[v] = pCrawl[1]
+                    parent[v] = u
+                    min_heap.decrease_key(v, key[v])
+
+        print_array(parent, V)
---
+++ datastructures/MinHeap.py
@@ -0,0 +1,74 @@
+class MinHeap:
+
+    def __init__(self):
+        self.array = []
+        self.pos = []
+        self.size = 0
+
+    @staticmethod
+    def new_min_heap_node(v, dist):
+        return [v, dist]
+
+    def swap_min_heap_node(self, a, b):
+        t = self.array[a]
+        self.array[a] = self.array[b]
+        self.array[b] = t
+
+    def min_heapify(self, idx):
+        smallest = idx
+        left = 2 * idx + 1
+        right = 2 * idx + 2
+
+        if left < self.size and self.array[left][1] < \
+                self.array[smallest][1]:
+            smallest = left
+
+        if right < self.size and self.array[right][1] < \
+                self.array[smallest][1]:
+            smallest = right
+
+        if smallest != idx:
+            self.pos[self.array[smallest][0]] = idx
+            self.pos[self.array[idx][0]] = smallest
+            self.swap_min_heap_node(smallest, idx)
+            self.min_heapify(smallest)
+
+    def extract_min(self):
+        if self.is_empty():
+            return
+
+        root = self.array[0]
+
+        last_node = self.array[self.size - 1]
+        self.array[0] = last_node
+        self.pos[last_node[0]] = 0
+        self.pos[root[0]] = self.size - 1
+        self.size -= 1
+        self.min_heapify(0)
+
+        return root
+
+    def is_empty(self):
+        return True if self.size == 0 else False
+
+    def decrease_key(self, v, dist):
+        i = self.pos[v]
+        self.array[i][1] = dist
+
+        while i > 0 and self.array[i][1] < self.array[(i - 1) // 2][1]:
+            self.pos[self.array[i][0]] = (i - 1) / 2
+            self.pos[self.array[(i - 1) // 2][0]] = i
+            self.swap_min_heap_node(i, (i - 1) // 2)
+
+            i = (i - 1) // 2
+
+    def is_in_minheap(self, v):
+        if self.pos[v] < self.size:
+            return True
+        return False
+
+
+def print_array(parent, n):
+    for i in range(1, n):
+        print("% d - %d" % (parent[i], i))
+
